9a8d47696e2a2d63315d067c13c339af3c9e80a5
hunghg255 <hung.hg@hesperiafreepress.com>
2022-01-16 15:23:15 +0700

    update

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,10 +1,15 @@
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
 
-export const firebaseApp = initializeApp(
-  JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG as string)
-);
+export const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MSG as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+});
 
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
