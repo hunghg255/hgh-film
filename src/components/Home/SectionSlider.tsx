4f96879488a1cb18b885db36858f433b70d7cede
hunghg255 <hung.hg@hesperiafreepress.com>
2022-01-25 11:56:40 +0700

    fix

--- src/components/Home/SectionSlider.tsx
+++ src/components/Home/SectionSlider.tsx
@@ -1,10 +1,10 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { FC } from "react";
-import { IMAGE_CARD_SIZE } from "../../shared/constants";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Link } from "react-router-dom";
-import { Navigation } from "swiper";
+import { FC } from 'react';
+import { IMAGE_CARD_SIZE } from '../../shared/constants';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from 'react-router-dom';
+import { Navigation } from 'swiper';
 
 interface SliderProps {
   images: {
@@ -16,11 +16,13 @@ interface SliderProps {
 }
 
 const Slider: FC<SliderProps> = ({ images, coverType }) => {
+  if (!coverType) return null;
+
   return (
     <Swiper
       modules={[Navigation]}
       navigation
-      slidesPerView="auto"
+      slidesPerView='auto'
       slidesPerGroupAuto
       spaceBetween={30}
     >
@@ -30,20 +32,20 @@ const Slider: FC<SliderProps> = ({ images, coverType }) => {
           key={item.image}
         >
           <Link to={item.link}>
-            <div className="rounded-lg overflow-hidden bg-dark-lighten group">
+            <div className='rounded-lg overflow-hidden bg-dark-lighten group'>
               <LazyLoadImage
                 style={{
                   width: IMAGE_CARD_SIZE[coverType].width,
                   height: IMAGE_CARD_SIZE[coverType].height,
                 }}
-                className="group-hover:brightness-75 transition duration-300 object-cover"
+                className='group-hover:brightness-75 transition duration-300 object-cover'
                 src={item.image}
                 width={IMAGE_CARD_SIZE[coverType].width}
                 height={IMAGE_CARD_SIZE[coverType].height}
-                effect="opacity"
-                alt=""
+                effect='opacity'
+                alt=''
               />
-              <h1 className="group-hover:text-primary transition duration-300 pb-1 px-2 m-0 text-lg max-w-full whitespace-nowrap overflow-hidden text-ellipsis">
+              <h1 className='group-hover:text-primary transition duration-300 pb-1 px-2 m-0 text-lg max-w-full whitespace-nowrap overflow-hidden text-ellipsis'>
                 {item.title}
               </h1>
             </div>
